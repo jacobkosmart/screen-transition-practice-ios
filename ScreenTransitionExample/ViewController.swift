90710ec14b1c8597ef13c3d8c5a619ed22d1ab96
jacobkosmart <jacobkosmart@gmail.com>
2021-11-24 10:17:31 +0900

    Initial Commit

---
+++ ScreenTransitionExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransitionExample
+//
+//  Created by Jacob Ko on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
